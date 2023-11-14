8c4d421da96e769a7b590d4504e58314f99d980c
Jerry Arhawho <jerryarhawho@Jerrys-MacBook-Air.local>
2023-11-14 12:14:32 +0000

    Initial project setup

---
+++ src/main/java/com/jayklef/unix_rentals/UnixRentalsApplication.java
@@ -0,0 +1,13 @@
+package com.jayklef.unix_rentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnixRentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnixRentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayklef/unix_rentals/UnixRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayklef.unix_rentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnixRentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
