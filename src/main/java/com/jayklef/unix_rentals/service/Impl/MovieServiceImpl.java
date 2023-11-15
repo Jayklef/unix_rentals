743032f2457999f940f20a04b3ec479e2f2a2ddc
Jerry Arhawho <jerryarhawho@gmail.com>
2023-11-15 13:51:05 +0000

    MovieRes

--- src/main/java/com/jayklef/unix_rentals/service/Impl/MovieServiceImpl.java
+++ src/main/java/com/jayklef/unix_rentals/service/Impl/MovieServiceImpl.java
@@ -1,13 +1,38 @@
 package com.jayklef.unix_rentals.service.Impl;
 
 import com.jayklef.unix_rentals.dto.MovieDto;
+import com.jayklef.unix_rentals.entity.Movie;
+import com.jayklef.unix_rentals.repository.MovieRepository;
 import com.jayklef.unix_rentals.service.MovieService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieServiceImpl implements MovieService {
+
+    private MovieRepository movieRepository;
+
+    public MovieServiceImpl(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
     @Override
     public MovieDto saveMovie(MovieDto movieDto) {
-        return null;
+
+        Movie movie = new Movie();
+        movie.setId(movieDto.getId());
+        movie.setTitle(movieDto.getTitle());
+        movie.setDirector(movieDto.getDirector());
+        movie.setReleaseYear(movieDto.getReleaseYear());
+
+        Movie newMovie = movieRepository.save(movie);
+
+        MovieDto movieResponse = new MovieDto();
+        movieResponse.setId(newMovie.getId());
+        movieResponse.setTitle(newMovie.getTitle());
+        movieResponse.setDirector(newMovie.getDirector());
+        movieResponse.setReleaseYear(newMovie.getReleaseYear());
+
+        return movieResponse;
+
     }
 }
